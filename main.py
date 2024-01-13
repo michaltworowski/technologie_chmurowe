f63873a190a7a2f411fa2988356b84f0ff079de8
Michał Tworowski <michal.tworowski@edu.uekat.pl>
2024-01-13 15:57:02 +0100

    Program do detekcji osob

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+#import imutils
+
+# Initializing the HOG person
+# detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# Reading the Image
+img = cv2.imread('grupa_osob.jpg')
+img = cv2.resize(img, (700,400))
+
+# Resizing the Image
+#image = imutils.resize(img,
+					#width=min(400, image.shape[1]))
+
+# Detecting all the regions in the
+# Image that has a pedestrians inside it
+(rects, weights) = hog.detectMultiScale(img,
+									winStride=(4, 4),
+									padding=(8, 8),
+									scale=1.15)
+
+# Drawing the regions in the Image
+for (x, y, w, h) in rects:
+	cv2.rectangle(img, (x, y),
+				(x + w, y + h),
+				(0, 0, 255), 2)
+
+print(f'Found {len(rects)} humans')
+
+# Showing the output Image
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
