f42ede787b2c734ac83b49929916ff58232c111b
“Michał <michal.tworowski@edu.uekat.pl>
2024-01-14 11:50:56 +0100

    zmiany w projekcie

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 app = Flask(__name__)
 api = Api(app)
 
+
 class PeopleCounter(Resource):
-	def get(self):
-		# load image
-		image = cv2.imread('grupa_osob.jpg')
-		image = cv2.resize(image, (700, 400))
+    def get(self):
+        image = cv2.imread('grupa_osob.jpg')
+        image = cv2.resize(image, (700, 400))
 
-		# detect people in the image
-		(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+# detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-		return {'peopleCount': len(rects)}
+        return {'peopleCount': len(rects)}
 
 
 api.add_resource(PeopleCounter, '/')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
