ae9f9d1e5fc7a4dba7d769a1c706489c28358a37
“Michał <michal.tworowski@edu.uekat.pl>
2024-01-14 10:32:49 +0100

    zmiany w main py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 ﻿import cv2
+from flask import Flask
+from flask_restful import Resource, Api
 
-# Initializing the HOG person
-# detector
+# initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# Reading the Image
-img = cv2.imread('grupa_osob.jpg')
-img = cv2.resize(img, (700,400))
+app = Flask(__name__)
+api = Api(app)
 
-# Detecting all the regions in the
-# Image that has a pedestrians inside it
-(rects, weights) = hog.detectMultiScale(img,
-									winStride=(4, 4),
-									padding=(8, 8),
-									scale=1.15)
+class PeopleCounter(Resource):
+	def get(self):
+		# load image
+		image = cv2.imread('grupa_osob.jpg')
+		image = cv2.resize(image, (700, 400))
 
-# Drawing the regions in the Image
-for (x, y, w, h) in rects:
-	cv2.rectangle(img, (x, y),
-				(x + w, y + h),
-				(0, 0, 255), 2)
+		# detect people in the image
+		(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-print(f'Found {len(rects)} humans')
+		return {'peopleCount': len(rects)}
 
-# Showing the output Image
-cv2.imshow("Image", img)
-cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+api.add_resource(PeopleCounter, '/')
+
+if __name__ == '__main__':
+	app.run(debug=True)
